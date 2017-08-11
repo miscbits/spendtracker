859ed75dde24a25902ec21a3ae1473a5cb26980e
miscbits <willow6786@gmail.com>
2017-08-11 00:39:37 -0400

    added relationships to models

--- app/Item.php
+++ app/Item.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     protected $fillable = [
-        'name', 'type_id',
+        'name',
     ];
+
+    public function purchases() {
+        return $this->belongsToMany('App\Purchase');
+    }
+
+    public function types() {
+        return $this->belongsToMany('App\Type');
+    }
 }
--- app/Purchase.php
+++ app/Purchase.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Purchase extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'cost',
+    ];
+
+	public function items() {
+		return $this->belongsToMany('App\Item');
+	}
 }
